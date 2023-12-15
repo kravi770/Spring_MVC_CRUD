0b52495118dc187908195fed27fd875f2992befb
kravi770 <770rkravi@gmail.com>
2023-12-15 22:40:30 +0530

    Initial Commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+
+        List<Employee>  theEmployees=employeeService.findAll();
+        theModel.addAttribute("employees",theEmployees);
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormAdd")
+    public String showFormAdd(Model theModel){
+        Employee theEmployee=new Employee();
+        theModel.addAttribute("employee",theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId")int theId,Model theModel){
+        Employee theEmployee=employeeService.findById(theId);
+        theModel.addAttribute("employee",theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId")int theId){
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee")Employee theEmployee){
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
+
+}
